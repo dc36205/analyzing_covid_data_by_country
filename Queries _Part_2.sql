b1dcf7e92e024e14c64c1f400bd65b91daba3ce8
Daniel <dc36205@gmail.com>
2024-07-18 16:36:23 -0600

    Update Queries _Part_2.sql

--- Queries _Part_2.sql
+++ Queries _Part_2.sql
@@ -1,9 +1,45 @@
 --Queries Part 2
 
---Global numbers variant without the date
-Select SUM(new_cases)  as total_cases, SUM(new_deaths) as total_deaths, (SUM(new_deaths)/ nullif(SUM(new_cases),0))*100 as DeathsPercentage     
-From PortfolioProject..CovidDeaths2_some_clean_perform
+-- Working with Vaccination data
+
+--Select *
+--From PortfolioProject..CovidVaccination 
+
+
+--Merging both datasets together:
+--Select * --SUM(new_cases)  as total_cases, SUM(new_deaths) as total_deaths, (SUM(new_deaths)/ nullif(SUM(new_cases),0))*100 as DeathsPercentage     
+--From PortfolioProject..CovidDeaths2_some_clean_perform as dea
+--Join PortfolioProject..CovidVaccination  as vac
+--    On dea.location = vac.location
+--	and dea.date = vac.date
+
+
+
+-- Looking at Total Populaiton vs vaccination
+--One way
+Select dea.continent, dea.location,  dea.date,  dea.population, vac.new_vaccinations  --SUM(new_cases)  as total_cases, SUM(new_deaths) as total_deaths, (SUM(new_deaths)/ nullif(SUM(new_cases),0))*100 as DeathsPercentage     
+, SUM(cast (vac.new_vaccinations as int)) OVER (Partition by dea.location)
+From PortfolioProject..CovidDeaths2_some_clean_perform as dea
+Join PortfolioProject..CovidVaccination  as vac
+    On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
+order by 2, 3
+
+-- Looking at Total Populaiton vs vaccination
+--Second way
+--Select dea.continent, dea.location,  dea.date,  dea.population, vac.new_vaccinations  --SUM(new_cases)  as total_cases, SUM(new_deaths) as total_deaths, (SUM(new_deaths)/ nullif(SUM(new_cases),0))*100 as DeathsPercentage     
+--, SUM(CONVERT(int, vac.new_vaccinations)) OVER (Partition by dea.location)
+--From PortfolioProject..CovidDeaths2_some_clean_perform as dea
+--Join PortfolioProject..CovidVaccination  as vac
+--    On dea.location = vac.location
+--	and dea.date = vac.date
+--where dea.continent is not null
+--order by 2, 3
+
+
 --where Location like  '%states%'
-where continent is not null
+
 --Group by date
-order by 1, 2
+
+  
